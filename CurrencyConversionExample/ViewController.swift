a56490b53786bf32ce531bb5486bfa6f389ee337
Peter Witham <peter.witham@gmail.com>
2019-07-07 16:31:13 -0500

    Initial Commit

---
+++ CurrencyConversionExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConversionExample
+//
+//  Created by Peter Witham on 7/7/19.
+//  Copyright © 2019 Peter Witham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
