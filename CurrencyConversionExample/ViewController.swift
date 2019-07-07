d4460d02ab1411c56b6721880cdb29575af7fbf2
Peter Witham <peter.witham@gmail.com>
2019-07-07 16:58:06 -0500

    Added controls and function for working example.

--- CurrencyConversionExample/ViewController.swift
+++ CurrencyConversionExample/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    @IBOutlet weak var entryField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func convertButton(_ sender: UIButton) {
+        let numericValue = Int(entryField.text!)!
+        let currencyConversion = convertIntToCurrencyAsString(intValue: numericValue)
+        displayLabel.text = currencyConversion
+    }
+    
+    
+    func convertIntToCurrencyAsString(intValue: Int) -> String {
+        var stringVersion: String
+        let cFormatter = NumberFormatter()
+        cFormatter.usesGroupingSeparator = true
+        cFormatter.numberStyle = .currency
+        if let currencyString = cFormatter.string(from: NSNumber(value: intValue)) {
+            stringVersion = currencyString
+        } else {
+            stringVersion = "Invalid Message"
+        }
+        return stringVersion
     }
 
 
